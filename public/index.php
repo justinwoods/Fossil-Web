b131ea6f391e724495d51b568979901592ebfb86
Justin Woods <justin.a.woods@gmail.com>
2010-03-16 21:52:54 -0500

    application.ini: add autoloaderNamespaces for Fossil. public/index.php: set APPLICATION_ENV to development.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+define('APPLICATION_ENV', 'development');
+
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
