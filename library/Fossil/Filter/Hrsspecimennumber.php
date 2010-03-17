1c8e88052bb8ed1adc549a861cf52159f04da2d1
Justin Woods <justin.a.woods@gmail.com>
2010-03-16 22:04:12 -0500

    application/configs/application.ini: update database configuration field names to be compatible with automatic resource loading. application/views/scripts/index/index.phtml: display output. library/Fossil/Filter/Hrsspecimennumber.php: remove reference to deprecated method Zend_Filter::get() and replace it with Zend_Filter::filterStatic().

--- library/Fossil/Filter/Hrsspecimennumber.php
+++ library/Fossil/Filter/Hrsspecimennumber.php
@@ -6,7 +6,7 @@ class Fossil_Filter_Hrsspecimennumber implements Zend_Filter_Interface
     public function filter($value)
     {
 
-        $digits = Zend_Filter::get($value, 'Digits');
+        $digits = Zend_Filter::filterStatic($value, 'Digits');
         $padded = str_pad($digits, 5, '0', STR_PAD_LEFT);
         $output = 'HRS'.$padded;
         return $output;
